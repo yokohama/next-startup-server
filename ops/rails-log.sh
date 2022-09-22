650a008cca3571f1256c59633e8e9ed93535941b
yokohama <yuhei.yokohama@gmail.com>
2022-09-23 02:02:32 +0900

    init

--- ops/rails-log.sh
+++ ops/rails-log.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+#
+# Usage: ./ops/rails-log.sh [ local | dev | prod ]
+# params was required.
+#
 
-LOG_GROUP_NAME=`aws logs describe-log-groups --query 'logGroups[*].logGroupName' | jq -r '.[]' | grep local | grep Task`
+LOG_GROUP_ENV=$1
+LOG_PREFIX=NextStartupStack-${LOG_GROUP_ENV}-TaskDefinitionContainerLogGroup
+
+LOG_GROUP_NAME=`aws logs describe-log-groups --query 'logGroups[*].logGroupName' | 
+  jq -r '.[]' | 
+  grep ${LOG_PREFIX}`
+
 aws logs tail --since 1h --follow ${LOG_GROUP_NAME}
